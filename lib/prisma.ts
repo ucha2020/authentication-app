64a78fd747a104205ddccdeaf7e8a38470c2e55e
ucha <uchaokwudili@gmaul.com>
2026-02-24 18:22:43 +0100

    ok3

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
